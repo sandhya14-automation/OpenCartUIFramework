e59f3db5e6132fd4e8aef1843077ad7a1c87603d
sandhya.t <sandhya1412@gmail.com>
2025-09-07 15:06:11 -0600

    cartpage and cartpage test added

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+	int num = 10;
+	
+	public void numOfItemsInTheCart() {
+		System.out.println("Number of items in the cart: " + num);
+	}
+}
